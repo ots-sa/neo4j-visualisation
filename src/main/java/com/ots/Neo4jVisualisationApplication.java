ce2768e2f6cff91997b65030d942502bb6a039c6
sdimitriadis <dimitriadis.stefan@gmail.com>
2016-12-12 15:07:28 +0200

    Μετονομασία PersonGraph σε Neo4jVisualisation

--- src/main/java/com/ots/Neo4jVisualisationApplication.java
+++ src/main/java/com/ots/Neo4jVisualisationApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonGraphApiApplication {
+public class Neo4jVisualisationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonGraphApiApplication.class, args);
+		SpringApplication.run(Neo4jVisualisationApplication.class, args);
 	}
 }
